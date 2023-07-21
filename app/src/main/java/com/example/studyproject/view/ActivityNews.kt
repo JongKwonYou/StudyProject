7f4db93c514bae7efa77904acd477fdc05c38724
YJK <yjk.dev.9866@healthport.co.kr>
2023-07-21 15:18:11 +0900

    feat : git message convention check(3)

--- app/src/main/java/com/example/studyproject/view/ActivityNews.kt
+++ app/src/main/java/com/example/studyproject/view/ActivityNews.kt
@@ -13,7 +13,7 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-// git commit message convention check4
+// git commit message convention check6
 @AndroidEntryPoint
 class ActivityNews : AppCompatActivity() {
 
