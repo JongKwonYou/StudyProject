86e1694d62c0451ad3999130fd0f16ede109d6b2
YJK <yjk.dev.9866@healthport.co.kr>
2023-07-21 15:36:52 +0900

    feat : git commit message convention (15)

--- app/src/main/java/com/example/studyproject/MainActivity.kt
+++ app/src/main/java/com/example/studyproject/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.studyproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-//test2
+//test4
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
